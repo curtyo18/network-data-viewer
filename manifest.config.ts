cb44b0ba4fbee221214ffbe794f52109b6e7333e
curtyo18 <curtyo18@gmail.com>
2026-05-26 16:40:29 +0100

    chore(release): bump to v0.2.0

--- manifest.config.ts
+++ manifest.config.ts
@@ -3,7 +3,7 @@ import { defineManifest } from "@crxjs/vite-plugin";
 export default defineManifest({
   manifest_version: 3,
   name: "Network Data Viewer",
-  version: "0.1.0",
+  version: "0.2.0",
   minimum_chrome_version: "116",
   description: "Configurable network-data capture and analysis.",
   permissions: ["storage", "sidePanel", "offscreen"],
